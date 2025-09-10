7820341922825b7c17884bdbc0fbfaa97fe5f60a
DuboseCM <dubosecaremgmt@gmail.com>
2025-09-10 14:40:30 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,9 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;\f1\froman\fcharset0 Times-Italic;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red143\green144\blue150;\red147\green0\blue147;
-\red42\green44\blue51;\red66\green147\blue62;\red167\green87\blue5;\red50\green94\blue238;\red219\green63\blue57;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true
+  }
 }
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c62745\c63137\c65490;\cssrgb\c65098\c14902\c64314;
-\cssrgb\c21961\c22745\c25882;\cssrgb\c31373\c63137\c30980;\cssrgb\c71765\c41961\c392;\cssrgb\c25098\c47059\c94902;\cssrgb\c89412\c33725\c28627;
-}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
 
-\f0\fs24 \cf0 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 javascript\
-\pard\pardeftab720\partightenfactor0
-
-\f1\i \cf3 \strokec3 /** \cf4 \strokec4 @type\cf3 \strokec3  \cf5 \strokec5 \{\cf4 \strokec4 import\cf5 \strokec5 (\cf6 \strokec6 'next'\cf5 \strokec5 ).\cf7 \strokec7 NextConfig\cf5 \strokec5 \}\cf3 \strokec3  */
-\f0\i0 \cf5 \strokec5 \
-\pard\pardeftab720\partightenfactor0
-\cf4 \strokec4 const\cf5 \strokec5  nextConfig \cf8 \strokec8 =\cf5 \strokec5  \{\
-  \cf9 \strokec9 output\cf8 \strokec8 :\cf5 \strokec5  \cf6 \strokec6 'standalone'\cf5 \strokec5 ,\
-  \cf9 \strokec9 images\cf8 \strokec8 :\cf5 \strokec5  \{\
-    \cf9 \strokec9 unoptimized\cf8 \strokec8 :\cf5 \strokec5  \cf7 \strokec7 true\cf5 \strokec5 \
-  \}\
-\}\
-\
-module.exports \cf8 \strokec8 =\cf5 \strokec5  nextConfig\
-}
+module.exports = nextConfig
