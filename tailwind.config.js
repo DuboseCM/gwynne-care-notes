d279901150b0c9aeb517632c881468d63e763dbd
DuboseCM <dubosecaremgmt@gmail.com>
2025-09-10 13:26:49 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;\f1\froman\fcharset0 Times-Italic;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red143\green144\blue150;\red147\green0\blue147;
+\red42\green44\blue51;\red66\green147\blue62;\red167\green87\blue5;\red50\green94\blue238;\red219\green63\blue57;
+}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c62745\c63137\c65490;\cssrgb\c65098\c14902\c64314;
+\cssrgb\c21961\c22745\c25882;\cssrgb\c31373\c63137\c30980;\cssrgb\c71765\c41961\c392;\cssrgb\c25098\c47059\c94902;\cssrgb\c89412\c33725\c28627;
+}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf0 \expnd0\expndtw0\kerning0
+javascript\
+\
+\pard\pardeftab720\partightenfactor0
+
+\f1\i \cf3 /** \cf4 @type\cf3  \cf5 \{\cf4 import\cf5 (\cf6 'tailwindcss'\cf5 ).\cf7 Config\cf5 \}\cf3  */
+\f0\i0 \cf5 \
+module.exports \cf8 =\cf5  \{\
+  \cf9 content\cf8 :\cf5  [\
+    \cf6 './pages/**/*.\{js,ts,jsx,tsx,mdx\}'\cf5 ,\
+    \cf6 './components/**/*.\{js,ts,jsx,tsx,mdx\}'\cf5 ,\
+    \cf6 './app/**/*.\{js,ts,jsx,tsx,mdx\}'\cf5 ,\
+  ],\
+  \cf9 theme\cf8 :\cf5  \{\
+    \cf9 extend\cf8 :\cf5  \{\},\
+  \},\
+  \cf9 plugins\cf8 :\cf5  [],\
+\}\
+}
